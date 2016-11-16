e028c8d7023e2f93543ca5162a424e0c937ba37e
Piyush Toshniwal <piyush.toshniwal@hashedin.com>
2016-11-16 17:00:42 +0530

    removed dependency from app views for test cases

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
-import unittest
-from tests.test_views import SqlApiViewTest
-
-def all_tests():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(SqlApiViewTest))
-    return suite
+# import unittest
+# from tests.test_views import SqlApiViewTest
+#
+# def all_tests():
+#     suite = unittest.TestSuite()
+#     suite.addTest(unittest.makeSuite(SqlApiViewTest))
+#     return suite
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,18 +1,40 @@
 from __future__ import unicode_literals
 from django.test import TestCase, RequestFactory
-import os, sys
-root = os.path.dirname(__file__)
-sys.path.append(root + "/../example/exampleapp")
+from squealy.exception_handlers import RequiredParameterMissingException
+from squealy.views import SqlApiView
 
-from views import DatabaseTableReport
 
-class SqlApiViewTest(TestCase):
+class TestDatabaseTableReport(SqlApiView):
+    query = "select name, sql, 5 as num, 123 as some_column from sqlite_master limit 4;"
+    format = "table"
+    columns = {
+        "name": {
+            "type": "dimension",
+        },
+        "sql": {
+            "type": "dimension",
+        },
+        "num": {
+            "type": "metric",
+        }
+    }
+    transformations = [
+                       {"name": "merge", "kwargs": {"columns_to_merge": ["sql","some_column"], "new_column_name": "merged_column"}},
+                        {"name": "split", "kwargs": {"pivot_column": "name"}}
+    ]
+    parameters = {"name": { "type": "string"},
+                  "date": {"type": "date", "format": "YYYY/MM/DD"},
+                  "datetime": {"type": "datetime", "format": "YYYY/MM/DD HH:mm:ss"}}
+
+
+class UnitTest(TestCase):
     def test_get(self):
-        # factory = APIRequestFactory()
-        # request = factory.get('/example/table-report/')
         factory = RequestFactory()
         request = factory.get('/example/table-report/?name=testname&date=2008/09/08&datetime=2016/12/28%2012:12:12')
-        response = DatabaseTableReport.as_view()(request)
+        response = TestDatabaseTableReport.as_view()(request)
         self.assertEqual(response.status_code, 200)
-        #response.render()
-        #print response.data
+
+    def test_get1(self):
+        factory = RequestFactory()
+        request = factory.get('/example/table-report/?name=testname&date=2008/09/08')
+        self.assertRaises(RequiredParameterMissingException, TestDatabaseTableReport.as_view(), request)
