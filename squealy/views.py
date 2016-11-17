44ea223970f27d779a8daac39123a689765d1b05
Devashish Sharma <devashish.sharma@hashedin.com>
2016-11-17 12:32:43 +0530

    Revert "Removed print statement"
    This reverts commit f8cd6262585861e89d0b979d2dc97e17d3e1e7e8.

--- squealy/views.py
+++ squealy/views.py
@@ -34,6 +34,7 @@ class SqlApiView(APIView):
         params = request.GET.copy()
         if hasattr(self, 'access_control'):
             session = self.extract_session_from_jwt(request)
+            print session
             self._validate_permissions(session)
 
         if hasattr(self, 'parameters'):
