a761bb5207ad62b0b8e1b6af3743ee060bb5eb95
Devashish Sharma <devashish.sharma@hashedin.com>
2016-11-25 18:25:56 +0530

    Fixing Travis Build Failure

--- tests/test_views.py
+++ tests/test_views.py
@@ -304,7 +304,3 @@ class TestYamlApiGeneration(TestCase):
         response = self.squealy_urls[3].resolve('api4').func(request)
         response.render()
         self.assertEqual(response.status_code, 200)
-        yesterday = datetime.datetime.now().date()-datetime.timedelta(days=1)
-        print type(response.data.get('data')[0][3].split(' ',1)[0].__str__())
-        print type(yesterday.__str__()), yesterday.__str__()
-        self.assertEqual(response.data.get('data')[0][3].split(' ',1)[0].__str__(), yesterday.__str__())
