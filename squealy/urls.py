0116ccab842fd40ce5d5aad27aa967a8090f4a8c
Vaibhav Singh <vaibhav.singh@hashedin.com>
2016-12-05 22:14:47 +0530

    Fixing py files

--- squealy/urls.py
+++ squealy/urls.py
@@ -3,6 +3,7 @@ from squealy import views
 
 urlpatterns = [
     url(r'^squealy/$', views.squealy_interface),
+    url(r'^yaml-generator/$', views.YamlGeneratorView.as_view()),
     url(r'^test/$', views.SqlApiView.as_view()),
     url(r'^database-details/', views.DatabaseView.as_view())
 ]
