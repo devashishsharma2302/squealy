b021383cf5e58c631d1da8a8a679def5a5e01424
Devashish Sharma <devashish.sharma@hashedin.com>
2016-12-09 16:06:38 +0530

    Fixing Travis Mysql settings

--- example/example/settings.py
+++ example/example/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hiway',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
