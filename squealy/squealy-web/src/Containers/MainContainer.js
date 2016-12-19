b64d2b93a111c285d18c37c67c91a7e5ecf17399
Vaibhav Singh <vaibhav.singh@hashedin.com>
2016-12-19 15:51:00 +0530

    Fixed apiParams not defined error

--- squealy/squealy-web/src/Containers/MainContainer.js
+++ squealy/squealy-web/src/Containers/MainContainer.js
@@ -40,15 +40,16 @@ export class MainContainer extends Component {
   initializeStates = () => {
     if (!this.state.apiDefinition.length) {
       let tempApiDef = [getEmptyApiDefinition()]
-      this.setState({apiDefinition: tempApiDef})
+      let testData = [getEmptyTestData()]
+      this.setState({apiDefinition: tempApiDef,testData: testData})
     }
 
     if (!this.state.testData.length) {
+
       let tempTestData = []
-      for(let index in this.state.apiDefinition) {
+      this.state.apiDefinition.map(()=>{
         tempTestData.push(getEmptyTestData())
-      }
-      
+      })
       this.setState({testData: tempTestData})
     }
 
