6144e684835e15ed07f18e3c83076cd1ac6e2658
Vaibhav Singh <vaibhav.singh@hashedin.com>
2017-01-03 16:23:28 +0530

    HIR-361

--- squealy/squealy-web/src/Components/ParamDefinitionModalWrapper.js
+++ squealy/squealy-web/src/Components/ParamDefinitionModalWrapper.js
@@ -17,7 +17,7 @@ export default class ParamDefinitionModalWrapper extends Component {
   }
   editParam = false
   editParamIndex = -1
-  
+
   paramFormatSelectionHandler = (value) => {
     this.setState({selectedValue: value})
   }
@@ -42,8 +42,6 @@ export default class ParamDefinitionModalWrapper extends Component {
 
   saveParamHandler = () => {
     let paramDef = this.props.selectedApiDefinition.paramDefinition.slice()
-
-
     if (this.state.isParamDefCustom && !this.refs.customParamPath.value) {
       console.error('Custom Param Definition Path is mandatory if paramDefinition type is selected as Custom')
       return
@@ -68,6 +66,7 @@ export default class ParamDefinitionModalWrapper extends Component {
     }
     this.props.handleEditParam(false, -1)
     this.props.onChangeApiDefinition('paramDefinition', paramDef)
+    this.props.closeParamModal()
   }
 
   getDefaultFunctionObj = () => {
@@ -100,9 +99,6 @@ export default class ParamDefinitionModalWrapper extends Component {
     this.editParamIndex = nextProps.editParamState.paramIndex
     if (nextProps.editParamState.editParam) {
       let selectedApiParamDef = nextProps.editParamState.selectedApiParamDef
-      this.refs.varName.value = selectedApiParamDef.name
-      this.refs.mandatoryField.checked = !selectedApiParamDef.optional
-      this.refs.defaultValues.value = selectedApiParamDef.default_values
       if (this.refs.hasOwnProperty('selectedValueFormat')) {
         this.refs.selectedValueFormat.value = selectedApiParamDef.kwargs ? selectedApiParamDef.kwargs.format : ''
       }
@@ -114,6 +110,19 @@ export default class ParamDefinitionModalWrapper extends Component {
         this.setState({isParamDefCustom: false}, () => {
           this.setState({selectedValue: selectedApiParamDef.type})
         })
+      }
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.editParam = this.props.editParamState.editParam
+    this.editParamIndex = this.props.editParamState.paramIndex
+    if (this.props.editParamState.editParam) {
+      if (this.refs) {
+        let selectedApiParamDef = this.props.editParamState.selectedApiParamDef
+        this.refs.varName.value = selectedApiParamDef.name
+        this.refs.mandatoryField.checked = !selectedApiParamDef.optional
+        this.refs.defaultValues.value = selectedApiParamDef.default_values
       }
     }
   }
@@ -135,6 +144,7 @@ export default class ParamDefinitionModalWrapper extends Component {
     this.setState({showValidationForm: false, query: ''})
     this.editValidationMode = false
     this.selectedValidatorIndex = null
+    this.props.closeValidationModal()
   }
 
   deleteEntry = (index, fieldName) => {
@@ -149,8 +159,8 @@ export default class ParamDefinitionModalWrapper extends Component {
       this.refs.errorMessage.value = validationObj[index].validation_function.kwargs.error_message
       this.refs.errorCode.value = validationObj[index].validation_function.kwargs.error_code
       if(validationObj[index].validation_function.kwargs) {
-        this.setState({query: validationObj[index].validation_function.kwargs.query}) 
-      } 
+        this.setState({query: validationObj[index].validation_function.kwargs.query})
+      }
       else {
         this.refs.validationFunc.value = validationObj[index].validation_function.name
       }
@@ -163,9 +173,8 @@ export default class ParamDefinitionModalWrapper extends Component {
     const {selectedApiDefinition, onChangeApiDefinition, editParam} = this.props
     let paramDefinition = selectedApiDefinition.paramDefinition,
       validations = selectedApiDefinition.validations
-
     //Modal body for Add Param modal
-    let addParamModalContent = 
+    let addParamModalContent =
       <div className='row add-modal-content'>
         <div className="validation-options">
           <label className='radio-inline'>
@@ -184,7 +193,7 @@ export default class ParamDefinitionModalWrapper extends Component {
           this.state.isParamDefCustom ? <div className='col-md-12'>
             <label htmlFor='customParamPath' className='col-md-4'>Custom Param Definition Path: </label>
             <input type='text' name='customParamPath' id='customParamPath' ref='customParamPath'/>
-          </div> : 
+          </div> :
           <div className='col-md-12'>
             <label htmlFor='paramFormat' className='col-md-4'>Param Definition Format: </label>
             <HidashDropdown
@@ -212,7 +221,7 @@ export default class ParamDefinitionModalWrapper extends Component {
       </div>
 
     //Modal body for Add Validations modal
-    let addValidationsModalContent = 
+    let addValidationsModalContent =
       <div className='add-modal-content'>
         <div className='validation-table-wrapper'>
           <table className="table table-hover">
@@ -225,15 +234,15 @@ export default class ParamDefinitionModalWrapper extends Component {
               </tr>
             </thead>
             <tbody>
-              {validations.length ? 
+              {validations.length ?
                 validations.map((validation, i) => {
                   return (
                     <tr key={'param_row_'+i}>
                       <td onClick={() => this.editValidation(i)} className='param-name'>{validation.validation_function.kwargs.error_message}</td>
                       <td>{validation.validation_function.kwargs.error_code}</td>
-                      <td>{validation.validation_function.kwargs ? validation.validation_function.kwargs.query : validation.validation_function.name}</td>
+                      <td>{validation.validation_function.kwargs.query ? validation.validation_function.kwargs.query : validation.validation_function.name}</td>
                       <td className='delete-validation-button'><div><i className="fa fa-trash-o" aria-hidden="true" onClick={() =>this.deleteEntry(i, 'validations')}/></div></td>
-                    </tr> 
+                    </tr>
                   )
                 })
               : null}
@@ -242,7 +251,7 @@ export default class ParamDefinitionModalWrapper extends Component {
           </table>
         </div>
         {
-          this.state.showValidationForm ? 
+          this.state.showValidationForm ?
             <div className='row add-validation-form'>
               <div className='col-md-12'>
                 <h3>Add Validation</h3>
@@ -285,16 +294,14 @@ export default class ParamDefinitionModalWrapper extends Component {
                   <input type='text' name='validationFunc' ref='validationFunc'/>
               }
               </div>
-              <button type="button" className="btn btn-info" onClick={this.saveValidationHandler}>Add</button>
             </div>
           : null
         }
       </div>
-
     return  (<div>
-      <HidashModal modalId='addParamsModal' closeModal={this.props.closeParamModal} showModal={this.props.showParamModal} modalHeader='Add Parameters' 
+      <HidashModal modalId='addParamsModal' closeModal={this.props.closeParamModal} showModal={this.props.showParamModal} modalHeader='Add Parameters'
       modalContent={addParamModalContent} saveChanges={this.saveParamHandler}/>
-      <HidashModal modalId='addValidationsModal' closeModal={this.props.closeValidationModal} showModal={this.props.showValidationModal} modalHeader='Validations' 
+      <HidashModal modalId='addValidationsModal' closeModal={this.props.closeValidationModal} showModal={this.props.showValidationModal} modalHeader='Validations'
       modalContent={addValidationsModalContent} saveChanges={this.saveValidationHandler} modalSize="large"/>
       </div>)
   }
