f89886e9ff158434ee88c54cd7aed086a9ee05b0
Devashish Sharma <devashish.sharma@hashedin.com>
2017-02-10 19:24:36 +0530

    Fixed indentation issues

--- squealy/squealy-web/src/Components/temp/SideMenu.js
+++ squealy/squealy-web/src/Components/temp/SideMenu.js
@@ -22,18 +22,18 @@ export default class SideMenu extends Component {
 			    </ul>
 	      </div>
 	      <div className="dashboard-list">
-	      <div className="side-menu-heading"><i className="fa fa-pie-chart chart-icon" aria-hidden="true"></i><span>Dashboards</span>
-	      	<i className="fa fa-plus add-new" aria-hidden="true"></i>
-	      </div>
-	      <div className="chart-list">
-		      <ul>
-			      <li> dash-01 </li>
-			      <li> dash-02 </li>
-			      <li> dash-03 </li>
-			      <li> dash-04 </li>
-			      <li> dash-05 </li>
-		      </ul>
-	      </div>
+		      <div className="side-menu-heading"><i className="fa fa-pie-chart chart-icon" aria-hidden="true"></i><span>Dashboards</span>
+		      	<i className="fa fa-plus add-new" aria-hidden="true"></i>
+		      </div>
+		      <div className="chart-list">
+			      <ul>
+				      <li> dash-01 </li>
+				      <li> dash-02 </li>
+				      <li> dash-03 </li>
+				      <li> dash-04 </li>
+				      <li> dash-05 </li>
+			      </ul>
+		      </div>
 	      </div>
       </div>
   )}
