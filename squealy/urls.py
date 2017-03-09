f8ae89a885203f4cc0476db081e1b0160ad36f14
Swapnil Tiwari <swapnil.tiwari@hashedin.com>
2017-03-09 20:22:44 +0530

    HIR-437 Url config for multiple db

--- squealy/urls.py
+++ squealy/urls.py
@@ -4,8 +4,11 @@ from django.contrib.auth.decorators import login_required
 import views
 
 urlpatterns = [
-    url(r'charts/$', login_required(views.ChartsLoaderView.as_view())),
-    url(r'user/$', login_required(views.UserInformation.as_view())),
-    url(r'squealy/(?P<chart_url>[-\w]+)/$', login_required(views.ChartView.as_view())),
-    url(r'^', login_required(views.squealy_interface)),
+    url(r'charts/$', views.ChartsLoaderView.as_view()),
+    url(r'user/$', views.UserInformation.as_view()),
+    url(r'databases/$', views.DatabaseView.as_view()),
+    url(r'squealy/(?P<chart_url>[-\w]+)/$', views.ChartView.as_view()),
+    url(r'^$', views.squealy_interface),
+    url(r'^(?P<chart_name>[\w@%.\Wd]+)/$', views.squealy_interface),
+    url(r'^(?P<chart_name>[\w@%.\Wd]+)/(?P<mode>\w+)$', views.squealy_interface),
 ]
