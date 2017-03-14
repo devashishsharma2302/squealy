56c18670ab3abf16f4209828ffdfc3a02cb145a7
dakshgautam <daksh.gautam@hashedin.com>
2017-03-14 13:20:18 +0530

    HIR-442: Added HelpModal Component

--- squealy/squealy-web/src/Components/AccordionTab.js
+++ squealy/squealy-web/src/Components/AccordionTab.js
@@ -4,6 +4,7 @@ import { SquealyModal } from './SquealyUtilsComponents'
 import AceEditor from 'react-ace'
 import 'brace/mode/sql'
 import 'brace/theme/tomorrow'
+import HelpModal from './HelpModal'
 
 
 export default class AccordionTab extends Component {
@@ -31,7 +32,7 @@ export default class AccordionTab extends Component {
     this.setState({ showHelpModal: true })
   }
 
-  close = () => {
+  closeHelpModal = () => {
     this.setState({ showModal: false })
   }
   showModalInWindow = (event) => {
@@ -49,98 +50,7 @@ export default class AccordionTab extends Component {
 
   render() {
 
-    const codeLine1 = 'SELECT project, timesheet, hours FROM timesheet WHERE start_date = {{ params.start_date }}'
-    const codeLine2 = 'SELECT project, timesheet, hours FROM timesheet WHERE start_date = {{ params.start_date }} \n {% if params.project_id %} \n AND project_id = {{ params.project_id }} \n {% endif %}'
-    const codeLine3 = `{% macro dates(date1, date2) -%}
-   between {{ date1 }} and {{ date2 }}
-{%- endmacro  %}
-
-select
-{% if params.project_id == "HIN-HWAY-DEL" %}
-   CONCAT(u.first_name, " ", u.last_name) as "Resource",a.name as "Type", sum(b.hrs) as "Time Spent"
-   from(
-       select *
-       from timesheet_timesheet
-       where project_code={{params.project_id}}  and day {{ dates(params.start_date, params.end_date) }}
-   ) b
-   join timesheet_timesheettype a on a.id=b.type_id
-   join auth_user u on u.id=b.owner_id
-   group by u.username,a.name
-{% else %}
-   5
-{% endif %}`
-
-    const desciptionCodeLine1 = 'These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted These are the things that have to be adjusted '
-    const line2 = '{{ params.start_date }} is the variable that is being passed in the query . As soon as you write the query, an entry with name "start_date" is created in "parameter defination modal" . You can edit the defination of the entry and change the diffrent properties of the entry'
-    const line1 = 'To pass a variable in a query , you should place a variable in {{ }}'
-    const line3 = 'Squealy uses Jinja2 Templating Engine which allows us to use {% if %} tag and {% for %} tag'
-    const line4 = 'Jinja2 also provides a feature of adding a macro , that can also be extensivly used .'
-    const helpContent = (
-      <div className="helpContainer">
-        <div className="codeDescriptionContainer">
-          <AceEditor
-            mode="sql"
-            theme="tomorrow"
-            onChange={this.testFunction}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{ $blockScrolling: true }}
-            value={codeLine1}
-            wrapEnabled={true}
-            width="50%"
-            height="auto"
-            fontSize={15}
-            readOnly={true}
-            />
-          <div className="helpDescrition">
-            <ul>
-              <li><p>{line1}</p></li>
-              <li><p>{line2}</p></li>
-            </ul>
-          </div>
-        </div>
-        <div className="codeDescriptionContainer">
-          <AceEditor
-            mode="sql"
-            theme="tomorrow"
-            onChange={this.testFunction}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{ $blockScrolling: true }}
-            value={codeLine2}
-            wrapEnabled={true}
-            fontSize={15}
-            width="50%"
-            height="auto"
-            readOnly={true}
-            />
-          <div className="helpDescrition">
-            <ul>
-              <li><p> {line3}</p></li>
-            </ul>
-
-          </div>
-        </div>
-        <div className="codeDescriptionContainer">
-          <AceEditor
-            mode="sql"
-            theme="tomorrow"
-            onChange={this.testFunction}
-            name="UNIQUE_ID_OF_DIV"
-            editorProps={{ $blockScrolling: true }}
-            value={codeLine3}
-            wrapEnabled={true}
-            width="50%"
-            fontSize={15}
-            height="auto"
-            readOnly={true}
-            />
-          <div className="helpDescrition">
-            <ul>
-              <li><p>{line4}</p></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    )
+   
     const {heading} = this.props
 
     const newColor = 'red'
@@ -154,16 +64,14 @@ select
 
           }
           <i className={'fa fa-2x param-heading-icon ' + this.state.accordionHeaderIcon} />
-
-          <SquealyModal modalHeader={"This is help text"}
+          <HelpModal 
+            modalHeader={"Learn By Example"}
             modalId={1}
-            modalContent={helpContent}
             showModal={this.state.showModal}
             modalSize={"large"}
-            closeModal={this.close}
+            closeModal={this.closeHelpModal}
             dialogClassName={"helpModal"}
-            noFooter={null}
-            />
+          />
         </div>
       </div>)
     return (
---
+++ squealy/squealy-web/src/Components/HelpModal.js
@@ -0,0 +1,99 @@
+import React,{Component} from 'react'
+import {Accordian , Panel} from 'react-bootstrap'
+import { SquealyModal } from './SquealyUtilsComponents'
+import AceEditor from 'react-ace'
+import 'brace/mode/sql'
+import 'brace/theme/tomorrow'
+import { query1,query2,query3,descQuery1_1,descQuery1_2,descQuery2,descQuery3 } from './../Constant'
+
+export default class HelpModal extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+    
+    const helpContent = (
+      <div className="helpContainer">
+        <div className="codeDescriptionContainer">
+          <AceEditor
+            mode="sql"
+            theme="tomorrow"
+            onChange={this.testFunction}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            value={query1}
+            wrapEnabled={true}
+            width="50%"
+            height="auto"
+            fontSize={15}
+            readOnly={true}
+            />
+          <div className="helpDescrition">
+            <ul>
+              <li><p>{descQuery1_1}</p></li>
+              <li><p>{descQuery1_2}</p></li>
+            </ul>
+          </div>
+        </div>
+        <div className="codeDescriptionContainer">
+          <AceEditor
+            mode="sql"
+            theme="tomorrow"
+            onChange={this.testFunction}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            value={query2}
+            wrapEnabled={true}
+            fontSize={15}
+            width="50%"
+            height="auto"
+            readOnly={true}
+            />
+          <div className="helpDescrition">
+            <ul>
+              <li><p> {descQuery2}</p></li>
+            </ul>
+
+          </div>
+        </div>
+        <div className="codeDescriptionContainer">
+          <AceEditor
+            mode="sql"
+            theme="tomorrow"
+            onChange={this.testFunction}
+            name="UNIQUE_ID_OF_DIV"
+            editorProps={{ $blockScrolling: true }}
+            value={query3}
+            wrapEnabled={true}
+            width="50%"
+            fontSize={15}
+            height="auto"
+            readOnly={true}
+            />
+          <div className="helpDescrition">
+            <ul>
+              <li><p>{descQuery3}</p></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+
+        const {modalHeader,modalId,showModal,modalSize,closeModal,dialogClassName} = this.props
+        return (
+        <SquealyModal 
+            modalHeader={modalHeader}
+            modalId={modalId}
+            modalContent={helpContent}
+            showModal={showModal}
+            modalSize={modalSize}
+            closeModal={closeModal}
+            dialogClassName={dialogClassName}
+            noFooter={null}
+            />
+        )
+    }
+}
