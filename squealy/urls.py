65381141d71175d9c3dec6a08b822d5bff925c4f
Devashish Sharma <devashish.sharma@hashedin.com>
2017-03-17 12:44:01 +0530

    Fixing url pattern for squealy APIs

--- squealy/urls.py
+++ squealy/urls.py
@@ -6,7 +6,7 @@ import views
 urlpatterns = [
    url(r'charts/$', login_required(views.ChartsLoaderView.as_view())),
    url(r'user/$', login_required(views.UserInformation.as_view())),
-   url(r'squealy/(?P<chart_url>[-\w]+)/$', login_required(views.ChartView.as_view())),
+   url(r'squealy/(?P<chart_url>[-\w]+)', login_required(views.ChartView.as_view())),
    url(r'^$', login_required(views.squealy_interface)),
    url(r'^(?P<chart_name>[\w@%.\Wd]+)/$', login_required(views.squealy_interface)),
    url(r'^(?P<chart_name>[\w@%.\Wd]+)/(?P<mode>\w+)$', login_required(views.squealy_interface)),
