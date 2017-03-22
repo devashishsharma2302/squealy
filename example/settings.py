fab7f667c67cbaaf5e79910e0f1994656a7dc878
Devashish Sharma <devashish.sharma@hashedin.com>
2017-03-22 20:22:57 +0530

    HIR-465: fixing travis build

--- example/settings.py
+++ example/settings.py
@@ -82,7 +82,8 @@ WSGI_APPLICATION = 'example.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 # Database for reports generation. Pass the DATABASE_URL variable from environment.
-DATABASES = extract_dj_database_urls(os.environ.get('QUERY_DB'))
+if os.environ.get('QUERY_DB'):
+    DATABASES = extract_dj_database_urls(os.environ.get('QUERY_DB'))
 
 
 
