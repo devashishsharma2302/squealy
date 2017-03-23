c1d23758a7b7ad01385671e3601be8d51c88d16d
Swapnil Tiwari <swapnil.tiwari@hashedin.com>
2017-03-23 12:40:42 +0530

    Rectified urls.py

--- squealy/urls.py
+++ squealy/urls.py
@@ -3,24 +3,13 @@ from django.contrib.auth.decorators import login_required
 
 import views
 
-# urlpatterns = [
-#    url(r'charts/$', login_required(views.ChartsLoaderView.as_view())),
-#    url(r'user/$', login_required(views.UserInformation.as_view())),
-#    url(r'squealy/(?P<chart_url>[-\w]+)', login_required(views.ChartView.as_view())),
-#    url(r'databases/$', login_required(views.DatabaseView.as_view())),
-#    url(r'^send-email/$', login_required(views.send_email)),
-#    url(r'^$', login_required(views.squealy_interface)),
-#    url(r'^(?P<chart_name>[\w@%.\Wd]+)/$', login_required(views.squealy_interface)),
-#    url(r'^(?P<chart_name>[\w@%.\Wd]+)/(?P<mode>\w+)$', login_required(views.squealy_interface)),
-# ]
-
 urlpatterns = [
-    url(r'charts/$', views.ChartsLoaderView.as_view()),
-    url(r'user/$', views.UserInformation.as_view()),
-    url(r'databases/$', views.DatabaseView.as_view()),
-    url(r'^send-email/(?P<chart_url>[-\w]+)/$', views.temp_report),
-    url(r'squealy/(?P<chart_url>[-\w]+)/$', views.ChartView.as_view()),
-    url(r'^$', views.squealy_interface),
-    url(r'^(?P<chart_name>[\w@%.\Wd]+)/$', views.squealy_interface),
-    url(r'^(?P<chart_name>[\w@%.\Wd]+)/(?P<mode>\w+)$', views.squealy_interface),
+   url(r'charts/$', login_required(views.ChartsLoaderView.as_view())),
+   url(r'user/$', login_required(views.UserInformation.as_view())),
+   url(r'squealy/(?P<chart_url>[-\w]+)', login_required(views.ChartView.as_view())),
+   url(r'databases/$', login_required(views.DatabaseView.as_view())),
+   url(r'^send-email/(?P<chart_url>[-\w]+)/$', views.temp_report),
+   url(r'^$', login_required(views.squealy_interface)),
+   url(r'^(?P<chart_name>[\w@%.\Wd]+)/$', login_required(views.squealy_interface)),
+   url(r'^(?P<chart_name>[\w@%.\Wd]+)/(?P<mode>\w+)$', login_required(views.squealy_interface)),
 ]
