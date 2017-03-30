a0e738a8d64310b7b6fbd50edc266b1d48acac83
Swapnil Tiwari <swapnil.tiwari@hashedin.com>
2017-03-30 19:48:48 +0530

    HIR-494 Merge conflicts resolved

--- squealy/tasks.py
+++ squealy/tasks.py
@@ -3,7 +3,7 @@ import requests
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
-from squealy.views import send_emails
+from email_service import send_emails
 
 # this will run every minute, see http://celeryproject.org/docs/reference/celery.task.schedules.html#celery.task.schedules.crontab
 @periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))
