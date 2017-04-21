898dedbb9a79f045fe405647374bdb127b284109
Swapnil Tiwari <swapniltiwari43@gmail.com>
2017-04-21 19:25:50 +0530

    Reverting changes in views

--- squealy/views.py
+++ squealy/views.py
@@ -50,7 +50,7 @@ class DatabaseView(APIView):
                 if db != 'default':
                     database_response.append({
                       'value': db,
-                      'label': db
+                      'label': database[db]['OPTIONS']['display_name'] if 'OPTIONS' in database[db] and 'display_name' in database[db]['OPTIONS'] else db
                     })
             if not database_response:
                 raise DatabaseConfigurationException('No databases found. Make sure that you have defined database configuration in django admin')
